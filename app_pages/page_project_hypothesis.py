36b90ac12fb0f017eb63c9b91336698673980d6c
wondrousWebWorks <alberts.johann@gmail.com>
2021-10-21 13:51:12 +0000

    Create Project Hypothesis page

---
+++ app_pages/page_project_hypothesis.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def page_project_hypothesis_body():
+
+    st.write("### Hypothesis and Validation")
+
+
+    st.success(
+        f"* It is suspected that not all 80 feature variables affect house "
+        f"prices equally: Correct"
+        f"The correlation study at Customer Study supports the hypothesis "
+        f"and suggests that only 10 features show strong correlation with "
+        f"property prices.\n\n"
+    )

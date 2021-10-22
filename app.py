f4dc58e16557b703b0488cc693ba4600df711952
wondrousWebWorks <alberts.johann@gmail.com>
2021-10-22 15:59:50 +0000

    Add skeleton for Custom Sale Price Prediction page

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_customer_study import page_customer_study_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_predict_sale_price import page_predict_sale_price_body
+from app_pages.page_custom_prediction import page_custom_prediction
 
 # Create an instance of the app 
 app = MultiPage(app_name="HousePricePrediction")
@@ -15,6 +16,7 @@ app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Customer Study", page_customer_study_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
 app.add_page("Predict Property Sale Price", page_predict_sale_price_body)
+app.add_page("Custom Feature Sale Price Prediction", page_custom_prediction)
 
 # Run the  app
 app.run()
---
+++ app_pages/page_custom_prediction.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def page_custom_prediction():
+    st.write("### Make Custom Predictions")

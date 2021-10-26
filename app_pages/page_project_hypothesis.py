3c30a2f72abed13c34616df5b90fc970234bdb46
wondrousWebWorks <alberts.johann@gmail.com>
2021-10-26 14:19:59 +0000

    Add correlation info

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -6,9 +6,10 @@ def page_project_hypothesis_body():
     st.write("### Hypothesis and Validation")
 
     st.success(
-        f"* It is suspected that not all 80 feature variables affect house "
+        f"* I suspected that not all 80 feature variables affect house "
         f"prices equally: Correct\n"
         f"\nThe correlation study at Customer Study supports the hypothesis "
         f"and suggests that only 10 features show strong correlation with "
-        f"property prices.\n\n"
+        f"property prices. A strong correlation would be a value of greater "
+        f"than 0.5\n"
     )
